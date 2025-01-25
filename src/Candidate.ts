304d30594f673b0843791745922b0ff0cffb1de2
morganjtroper2 <morganjtroper@gmail.com>
2025-01-24 23:56:36 -0800

    Update Candidate.ts

--- src/Candidate.ts
+++ src/Candidate.ts
@@ -1,9 +1,1 @@
-export interface Candidate {
-    login: string;
-    name: string;
-    avatar_url: string;
-    company?: string;
-    email?: string;
-    location?: string;
-    html_url: string;
-  }
+
